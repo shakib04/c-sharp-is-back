62bbf197ca3ebd30bdcbfc23adac4a4e2d0d329b
shakib <mdshakibulalam@gmail.com>
2021-01-08 22:22:22 +0600

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestConsoleApplication
+{
+    internal class Person
+    {
+        Address a1;
+        string name;
+        string bloodGroup;
+        string[] languages;
+
+        internal Person(string name, string bloodGroup, Address a1, string[] languages)
+        {
+            this.name = name;
+            this.bloodGroup = bloodGroup;
+            this.languages = languages;
+            this.a1 = a1;
+        }
+
+        internal void Display()
+        {
+            Console.WriteLine("--Person Details--");
+            Console.WriteLine("Name: "+this.name);
+            Console.WriteLine("Blood Group: {0}", this.bloodGroup);
+            for (int i = 0; i < languages.Length; i++)
+            {
+                char temp = languages[i][0];
+                Console.Write(char.ToUpper(temp) + languages[i].Substring(1));
+                if (i+1 == languages.Length)
+                {
+
+                }
+                else
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine();
+            a1.showAddress();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TestConsoleApplication
+{
+    struct Address
+    {
+        short homeNumber;
+        short roadNumber;
+        byte postalCode;
+
+        internal Address(short homeNumber, short roadNumber, byte postalCode)
+        {
+            this.homeNumber = homeNumber;
+            this.roadNumber = roadNumber;
+            this.postalCode = postalCode;
+        }
+
+        internal void showAddress()
+        {
+            Console.WriteLine("Home Number: {0}", this.homeNumber);
+            Console.WriteLine("Road Number: {0}", this.roadNumber);
+            Console.WriteLine("Postal Code: {0}", this.postalCode);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address a1 = new Address(10, 20, 123);
+            string[] languages = { "bangla", "english", "german" };
+            Person p1 = new Person("Shakib", "B+", a1, languages);
+            p1.Display();
+        }
+    }
+}
